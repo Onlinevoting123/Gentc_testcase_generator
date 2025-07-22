512019a43d221c014327a4c5fb080b49cb31f1dc
HEMANTH <siddunani1405@gmail.com>
2025-07-22 17:08:56 +0530

    Update assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,6 +1,7 @@
 import arrow_icon from './arrow_icon.svg'
 import lock_icon from './lock_icon.svg'
 import logo from './logo.svg'
+import log from './logo.png'
 import mail_icon from './mail_icon.svg'
 import person_icon from './person_icon.svg'
 import hand_wave from './hand_wave.png'
@@ -13,6 +14,7 @@ const assets = {
     mail_icon,
     person_icon,
     hand_wave,
-    header_img
+    header_img,
+    log
 }
 export default assets;
