d44928c18c1e47c87290fc1bfc09884b5188bde3
HEMANTH <siddunani1405@gmail.com>
2025-07-22 15:44:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import GeminiRouter from "./Routes/GeminiRouter.js";
 const app=express();
 const port=process.env.PORT || 4000
 connectDB();
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://gentc-testcase-generator.vercel.app/']
 
 app.use(express.json());
 app.use(cookieParser());
