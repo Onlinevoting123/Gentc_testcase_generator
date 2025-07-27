bf23e71cd1aa2c33dcc4a9bb65535ad94520b730
HEMANTH <siddunani1405@gmail.com>
2025-07-27 14:24:58 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,17 @@ import Home from './pages/Home'
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Prompt from './pages/Prompt'
+import Edge from './pages/Edge'
+import Gfg from './pages/Gfg'
+import Interviewbit from './pages/Interviewbit'
+import Codechef from './pages/Codechef'
+import Codeforces from './pages/Codeforces'
+import Hackerrank from './pages/Hackerrank'
+import Atcoder from './pages/Atcoder'
+import Spoj from './pages/Spoj'
+import Binarysearch from './pages/Binarysearch'
+import Lintcode from './pages/Lintcode'
+
 
 const App = () => {
   return (
@@ -17,7 +28,17 @@ const App = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/emailverify' element={<Emailverify/>}/>
         <Route path='/resetpass' element={<Resetpass/>}/>
-        <Route path='/prompt' element={<Prompt/>}/>
+        <Route path='/e' element={<Edge/>}/>
+        <Route path='/e/prompt' element={<Prompt/>}/>
+        <Route path='/e/gfg' element={<Gfg/>}/>
+        <Route path='/e/interviewbit' element={<Interviewbit/>}/>
+        <Route path='/e/codechef' element={<Codechef/>}/>
+        <Route path='/e/binarysearch' element={<Binarysearch/>}/>
+        <Route path='/e/codeforces' element={<Codeforces/>}/>
+        <Route path='/e/atcoder' element={<Atcoder/>}/>
+        <Route path='/e/hackerrank' element={<Hackerrank/>}/>
+        <Route path='/e/spoj' element={<Spoj/>}/>
+        <Route path='/e/lintcode' element={<Lintcode/>}/>
       </Routes>
     </div>
   )
