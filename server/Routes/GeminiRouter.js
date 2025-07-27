0a854dd13c7f2af19ed69d199818459c7f7657a5
HEMANTH <siddunani1405@gmail.com>
2025-07-27 14:23:26 +0530

    Update GeminiRouter.js

--- server/Routes/GeminiRouter.js
+++ server/Routes/GeminiRouter.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { getUserPrompt } from '../controllers/GeminiControllers.js';
+import { getUserPrompt,gfg,interviewbit,lintcode,binarysearch,codeforces,codechef,atcoder,hackerrank,spoj } from '../controllers/GeminiControllers.js';
 
 const GeminiRouter=express.Router();
 
-GeminiRouter.post('/prompt',getUserPrompt);
+GeminiRouter.post('/e/prompt',getUserPrompt);
+GeminiRouter.post('/e/gfg',gfg);
+GeminiRouter.post('/e/interviewbit',interviewbit);
+GeminiRouter.post('/e/lintcode',lintcode);
+GeminiRouter.post('/e/binarysearch',binarysearch);
+GeminiRouter.post('/e/codeforces',codeforces);
+GeminiRouter.post('/e/codechef',codechef);
+GeminiRouter.post('/e/atcoder',atcoder);
+GeminiRouter.post('/e/hackerrank',hackerrank);
+GeminiRouter.post('/e/spoj',spoj);
 
 export default GeminiRouter;
